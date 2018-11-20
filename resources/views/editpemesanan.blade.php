9b63603030872ec488273fa84bd374d30696c009
nofadwiadelia <nofadwiadelia47@gmail.com>
2018-11-20 22:57:47 +0700

    revisi

--- resources/views/editpemesanan.blade.php
+++ resources/views/editpemesanan.blade.php
@@ -30,7 +30,7 @@
                             <label class="control-label col-md-3 col-sm-3 col-xs-12" for="alamat">Alamat
                             </label>
                             <div class="col-md-6 col-sm-6 col-xs-12">
-                              <input type="text" id="alamat" name="alamat" value="{{ $data->alamat}}" class="form-control col-md-7 col-xs-12">
+                              <textarea type="text" id="alamat" name="alamat" class="form-control col-md-7 col-xs-12">{{ $data->alamat}}</textarea>
                             </div>
                       </div>
                       <div class="form-group">
